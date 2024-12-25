9503d745e07df67cfda045ba6093e7d34152b8db
Hiep Tran <hieptt149@gmail.com>
2024-12-25 10:31:51 +0700

    Create custom route

--- my-project/gateway-server/src/main/java/com/hieptt149/gateway_server/GatewayServerApplication.java
+++ my-project/gateway-server/src/main/java/com/hieptt149/gateway_server/GatewayServerApplication.java
@@ -2,12 +2,32 @@ package com.hieptt149.gateway_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
 
+    @Bean
+    public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/eazybank/accounts/**")
+                        .filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://ACCOUNTS")
+                )
+                .route(p -> p.path("/eazybank/cards/**")
+                        .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://CARDS")
+                )
+                .route(p -> p.path("/eazybank/loans/**")
+                        .filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://LOANS")
+                )
+                .build();
+    }
 }
